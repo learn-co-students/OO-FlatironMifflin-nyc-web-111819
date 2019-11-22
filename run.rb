633b2e7d4069843b5c490c453cc21777ee813019
Shakil Abdin <shakil.abdin@gmail.com>
2019-11-22 13:29:03 -0500

    Done.

--- run.rb
+++ run.rb
@@ -5,6 +5,18 @@ require 'pry'
 
 #Test your code here
 
+man1 = Manager.new("michael", "paper", 39)
+man2 = Manager.new("tony", "software engineering", 28)
+
+emp1 = Employee.new("dwight", 85000.00, man1)
+emp2 = Employee.new("jim", 86000.00, man1)
+emp3 = Employee.new("pam", 90000.00, man1)
+emp4 = Employee.new("angela", 79000, man2)
+emp5 = Employee.new("stanley", 82000, man2)
+
+
+
+
 
 binding.pry
 puts "done"
