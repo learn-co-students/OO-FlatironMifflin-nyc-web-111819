14a1a679c61c70043c0d9a140dae373911c1dea9
MACorman <madelinecorman@gmail.com>
2019-11-22 16:59:31 -0500

    completed mifflin lab

--- lib/Manager.rb
+++ lib/Manager.rb
@@ -1,3 +1,34 @@
 class Manager
 
+    attr_accessor :name, :department, :age
+    @@all = []
+
+    def initialize(name, department, age)
+        @name = name
+        @department = department
+        @age = age
+        @@all << self 
+    end 
+
+    def self.all
+        @@all
+    end
+
+    def employees
+        Employee.all.select {|e| e.manager_name == self}
+    end
+
+    def hire_employee(name, salary)
+        Employee.new(name, salary, self)
+    end
+
+    def self.all_departments
+        @@all.map {|m| m.department}
+    end
+
+    def self.average_age
+        new_array = @@all.map {|m| m.age}
+        new_array.sum.to_f / new_array.length
+    end
+
 end
