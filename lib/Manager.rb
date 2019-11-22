ca728f7db065622d9421cabef3409a61d807c2e8
Alex Williams <alex_williams@protonmail.com>
2019-11-22 06:32:19 -0500

    Done.

--- lib/Manager.rb
+++ lib/Manager.rb
@@ -1,3 +1,28 @@
 class Manager
-
+    attr_accessor :name, :department, :age, :employees
+    @@all = []
+    def initialize(name, department, age)
+        @name = name
+        @department = department
+        @age = age
+    end
+    def self.all
+        @@all
+    end
+    def employees
+        Employee.all.select {|employee| employee.manager_name == self.name}
+    end
+    def hire_employee(employee_name, salary)
+        new = Employee.new(employee_name, salary)
+        Employee.all << new
+        new.department = self.department
+        new.manager_name = self.name
+    end
+    def self.all_departments
+        @@all.select{|manager| manager.department}
+    end
+    def self.average_age
+        array = @@all.select{|manager| manager.age}
+        array.reduce(:+)/array.length
+    end
 end
